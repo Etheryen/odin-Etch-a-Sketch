3d375d5d8178134c6c13ac9f0a09206385c12e9d
Etheryen <lukasz.ciwoniuk@gmail.com>
2022-03-26 21:38:47 +0100

    no custom resolution

---
+++ script.js
@@ -0,0 +1,48 @@
+let clear = document.getElementById('clear');
+
+let screen = document.getElementById('screen');
+
+let grid = document.createElement('div');
+grid.id = 'grid';
+
+let square = document.createElement('div');
+square.className = 'square';
+
+let x = 16;
+
+render = (x) => {
+  let newGrid = grid.cloneNode(true)
+  screen.appendChild(newGrid);
+  for(let i = 0; i < 256; i++) {
+    newGrid.appendChild(square.cloneNode(true));
+  }
+  
+  let squares = document.getElementsByClassName('square');
+  
+  Array.from(squares).forEach(s => {
+    s.addEventListener('mouseover', e => {
+      s.classList.add('hovered');
+    })
+  });
+}
+
+clear.onclick = e = () => {
+  let x = prompt('Enter grid size(default 16): ');
+  if(x != null) {
+  if(x === '') {x = '16'};
+  x = parseInt(x)
+  }
+
+  document.getElementById('grid').remove();
+
+  let squares = document.getElementsByClassName('square');
+
+  Array.from(squares).forEach(s => {
+    s.addEventListener('mouseover', e => {
+      s.remove();
+    })
+  });
+  render(x);
+};
+
+render(x);
