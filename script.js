43e222ef12ec2815dc6dbcd33931ab503b075738
Etheryen <lukasz.ciwoniuk@gmail.com>
2022-03-28 21:56:58 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
-let clear = document.getElementById('clear');
+let clear = document.getElementById('clear')
 
-let screen = document.getElementById('screen');
+let screen = document.getElementById('screen')
 
-let grid = document.createElement('div');
-grid.id = 'grid';
+let grid = document.createElement('div')
+grid.id = 'grid'
 
-let square = document.createElement('div');
-square.className = 'square';
+let square = document.createElement('div')
+square.className = 'square'
 
-let x = 16;
+let x = 16
 
 render = (x) => {
   let newGrid = grid.cloneNode(true)
-  screen.appendChild(newGrid);
-  for(let i = 0; i < 256; i++) {
-    newGrid.appendChild(square.cloneNode(true));
+  screen.appendChild(newGrid)
+  for(let i = 0; i < x*x; i++) {
+    newGrid.appendChild(square.cloneNode(true))
   }
   
-  let squares = document.querySelectorAll('.square');
+  let squares = document.querySelectorAll('.square')
   
   squares.forEach(s => {
     s.addEventListener('mouseover', e => {
-      s.classList.add('hovered');
+      s.classList.add('hovered')
     })
-  });
+    s.style.width = `${800/x}px`
+    s.style.height = `${800/x}px`
+  })
 }
 
-clear.onclick = e = () => {
-  let x = prompt('Enter grid size(default 16): ');
-  if(x != null) {
-  if(x === '') {x = '16'};
+clear.onclick = () => {
+  let x = prompt('Enter grid size(default 16): ')
+  if(x == null) return
+  if(x === '') x = '16'
   x = parseInt(x)
-  }
+  if(x > 100) {x = 100}
+  
 
-  document.getElementById('grid').remove();
+  document.getElementById('grid').remove()
 
-  let squares = document.getElementsByClassName('square');
+  let squares = document.querySelectorAll('.square')
 
-  Array.from(squares).forEach(s => {
-    s.addEventListener('mouseover', e => {
-      s.remove();
+  squares.forEach(s => {
+    s.addEventListener('mouseover', () => {
+      s.remove()
     })
-  });
-  render(x);
-};
+  })
+  render(x)
+}
 
-render(x);
+render(x)
